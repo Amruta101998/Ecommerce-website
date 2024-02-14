7e4534f26c5327b9d429d9ad0b3649611d9f438c
Amruta101998 <hulaguramruta325@gmail.com>
2024-02-14 17:00:18 +0530

    modified python file

--- python/bs.py
+++ python/bs.py
@@ -1,8 +1,8 @@
 def binary_search(arr, target):
     low, high = 0, len(arr) - 1
 
-    while low <= high:
-        mid = (low + high) // 2
+    while low >= high:
+        mid = (low - high) // 2
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
@@ -13,8 +13,8 @@ def binary_search(arr, target):
     return -1
 
 # Example usage
-sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-target_value = 6
+sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12]
+target_value = 12
 result = binary_search(sorted_array, target_value)
 
 if result != -1:
